4ee402a1a5c49da24e5092c875550bd96f4d2b7b
anreh <anurehka@gmail.com>
2022-08-16 12:46:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a2pmpracw1/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.a2pmpracw1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.a2pmpracw1.databinding.ActivityMainBinding
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImg :ImageView
+    lateinit var numbText : TextView
+    lateinit var nameText : TextView
+    lateinit var playerNameTxt:TextView
+    lateinit var binding: ActivityMainBinding
+    private var gameInfo: GameInfo = GameInfo("kang", totalScore = "1")
+    private var totalScore: Int = gameInfo.totalScore.toInt()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        /*setContentView(R.layout.activity_main)
+        diceImg = findViewById(R.id.diceImage)
+        numbText = findViewById(R.id.numbTxt)
+        nameText = findViewById(R.id.EditPlayerName)
+        playerNameTxt = findViewById(R.id.playerName)*/
+
+
+        /*val rollBtn:Button = findViewById(R.id.rollButton)*/
+        binding.gameInfo = gameInfo
+        binding.rollButton.setOnClickListener { rollDice() }
+
+        /*val updateBtn:Button = findViewById(R.id.updateButton)*/
+
+        binding.updateButton.setOnClickListener { updateName(it) }
+    }
+
+    private fun updateName(view : View){
+        //binding.playerName.text = binding.EditPlayerName.text
+        binding.apply {
+            gameInfo?.playerName = EditPlayerName.text.toString()
+            invalidateAll()
+        }
+    }
+
+    private fun rollDice(){
+        val randNum = (1..6).random()
+//        val numberTxt : TextView = findViewById(R.id.numbTxt)
+        totalScore += randNum
+        binding.numbTxt.text = totalScore.toString()
+
+//        val diceImg : ImageView = findViewById(R.id.diceImage)
+        val imgSrc = when (randNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        binding.diceImage.setImageResource(imgSrc)
+    }
+}
